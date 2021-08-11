3087698abf62dd681059905dae210236f119f2c6
Michael Kagnew <mike.kagnew@gmail.com>
2021-08-11 16:34:57 -0400

    working header, just doing fancy stuff now

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,46 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from "../images/logo.png"
+import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = ({ siteTitle }) => 
+{ 
+  let [animate, setAnimate] = React.useState(true);
+
+  React.useEffect(()=> {
+
+    if(sessionStorage.getItem("fLoadKey") == null){
+      setAnimate(true);
+      sessionStorage.setItem("fLoadKey", 1);
+    } else {
+      setAnimate(false);
+    }
+  })
+
+  return (
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+       <Link className="navbar-brand" to="/"><img src={logo} alt="logo"></img></Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+          <li className="nav-item">
+             <Link to="/" className="nav-link" activeClassName="active">Home</Link>
+           </li>
+          <li className="nav-item">
+             <Link to="/using-typescript/" className="nav-link" activeClassName="active">Go to "Using TypeScript"</Link>
+           </li>
+           <li className="nav-item">
+             <Link to="/page-2/" className="nav-link" activeClassName="active">Page 2</Link>
+           </li>
+      </ul>
     </div>
-  </header>
-)
+  </div>
+</nav>
+)}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
